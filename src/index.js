edc59da4b21e460b49cb102bcd5c92fbcd8734b9
seolleung2 <seolleung22@gmail.com>
2021-01-11 20:56:05 +0900

    how to communicate with Reducer? The answer is store.dispatch({key:value})

--- src/index.js
+++ src/index.js
@@ -1,45 +1,33 @@
-import { createStore } from "redux"; // store 는 나의 data 를 넣는 곳이다. 나의 state 에 대한.
-// ! createStore 가 하는 일은 기본적으로 나의 data 를 넣을 수 있는 장소를 생성한다.
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.getElementById("number");
 
-// ! 뭐든지 modifier, reducer 가 리턴하는 건 나의 app data 가 된다. state 를 수정할 수 있는 함수는 이게 유일하다.
-const countModifier = (state = 0) => {
-  console.log(state);
-  return state; // reducer 가 리턴하는 것은 나의 app 에 있는 data 가 된다.
-}; // ! store 를 만들기 위해서는 reducer 가 필요하다고 한다.
-const countStore = createStore(countModifier); // Error: Expected the reducer to be a function. 아 안에 reducer 함수를 넣어줘야 하나보구만..
+const countModifier = (count = 0, action) => {
+  // ! reducer 함수 두번째 인자에는 action 이 오는데, countModifier 와 소통하기 위한 방법이 바로 action 이라 한다.
+  // console.log(count, action); // {type: "@@redux/INIT6.6.l.g.o.8"} 이게 액션이라고?
+  if (action.type === "ADD") {
+    // console.log("They are telling me to add one");
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
+const countStore = createStore(countModifier);
 
-// reducer (countModifier) 는 나의 state data 를 수정하는 (modify) 함수이다. count 를 올리거나 내리거나 하는 등의..
+// console.log(countStore.getState());
 
-console.log(countStore);
-
-// {dispatch: ƒ, subscribe: ƒ, getState: ƒ, replaceReducer: ƒ, Symbol(observable): ƒ}
-
+// ! 어떻게 count modifier 에게 action 을 보낼 수 있을까? (reducer 와 소통하기 위한 방법: action)
+// action should be an object.
+countStore.dispatch({ type: "ADD" }); // ! dispatch 를 통해 countModifier 로 메세지를 보내는 거다.
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 console.log(countStore.getState());
-
-// Let s Count Number! 가 출력된다.
-
-// ! state 는 현재 application 에서 변하는 data 를 의미한다. 여기서는 뭐가 state 인가?
-// let count = 0; // 바로 이것! count! 현재 app 에서 유일하게 바뀌는 코드.
-
-// number.innerText = count;
-
-// const updateText = () => {
-//   number.innerText = count; // ! html 에게 count 를 업데이트 하라고 알려주고 있다.
-// };
-
-// // count 를 수정하기 위해 handleAdd, handleMinus 함수를 작성했잖은가.
-// const handleAdd = () => {
-//   count = count + 1; // ! 얘만 있으면 실제 number.innerText 에 반영이 되지 않는다.
-//   updateText(); // ! 그래서 updateText 함수를 새로 만들어서 실제 값을 업데이트 하게 하였다.
-// };
-
-// const handleMinus = () => {
-//   count = count - 1;
-//   updateText();
-// };
-
-// add.addEventListener("click", handleAdd);
-// minus.addEventListener("click", handleMinus);
