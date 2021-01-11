c216b7154a4647697af991f219cad85af680e045
seolleung2 <seolleung22@gmail.com>
2021-01-11 21:34:25 +0900

    modified Reducer using switch, case and variable not string

--- src/index.js
+++ src/index.js
@@ -6,16 +6,27 @@ const number = document.getElementById("number");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
   // ! reducer 함수 두번째 인자에는 action 이 오는데, countModifier 와 소통하기 위한 방법이 바로 action 이라 한다.
   // console.log(count, action); // {type: "@@redux/INIT6.6.l.g.o.8"} 이게 액션이라고?
-  if (action.type === "ADD") {
-    // console.log("They are telling me to add one");
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  // if (action.type === "ADD") {
+  //   // console.log("They are telling me to add one");
+  //   return count + 1;
+  // } else if (action.type === "MINUS") {
+  //   return count - 1;
+  // } else {
+  //   return count;
+  // }
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 const countStore = createStore(countModifier);
@@ -31,11 +42,11 @@ countStore.subscribe(onChange); // ! store 에 변화가 생길 때 알려주는
 
 // ! countModifier (reducer) 에  action 을 전달하려면? store.dispatch({ key : value }) 를 사용한다.
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
