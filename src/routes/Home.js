f83e1a0c6117a65b58aa04a416bea944e0978f1a
seolleung2 <seolleung22@gmail.com>
2021-01-14 01:55:07 +0900

    make mapDispatchToProps to dispatch actions

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,8 +2,19 @@ import React, { useState } from "react";
 
 import { connect } from "react-redux";
 
+import { actionCreators } from "../store"; // ! 얘도 임포트 해와야 한다.
+
+// dispatch 를 home component 에서 사용하지 않는다. 함수를 만들어서 props 로 전달하게 한다.
 const Home = (props) => {
   console.log(props);
+  //   {history: {…}, location: {…}, match: {…}, staticContext: undefined, toDos: Array(0), …}
+  // addToDo: text => {…}
+  // history: {length: 5, action: "POP", location: {…}, createHref: ƒ, push: ƒ, …}
+  // location: {pathname: "/", search: "", hash: "", state: undefined}
+  // match: {path: "/", url: "/", isExact: true, params: {…}}
+  // staticContext: undefined
+  // toDos: []
+  // __proto__: Object
   const [text, setText] = useState("");
 
   const handleChange = (event) => {
@@ -12,8 +23,13 @@ const Home = (props) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    // ! 최종, 이제 내 home component 는 직접적으로 dispatch 나 action creators 를 처리할 필요가 없게 된다.
+    props.addToDo(text); // 성공적으로 reducer 에게 dispatch 하고 있다.
 
     setText("");
+    // props.dispatch(addToDo(text));
+    // ! 이렇게 props 로 받아와서 dispatch 를 날리면? 작동이 된다, 그러나? 이렇게 하는 것 대신 새로운 props 을 받아와 사용해보자.
+    // mapDispatchToProps 로 가서 수정해 보자.
   };
   return (
     <>
@@ -27,8 +43,32 @@ const Home = (props) => {
   );
 };
 
-const getCurrentState = (state, ownProps) => {
+// ! store.getState() 의 역할, props 로 state 를 받아오는 신박함이란..
+const mapStateToProps = (state, ownProps) => {
   return { toDos: state };
 };
 
-export default connect(getCurrentState)(Home);
+// ! 이제 어떻게 하면 component 가 dispatch 동작도 할 수 있을지 살펴보자. 즉 store.dispatch() 의 역할.
+// mapDispatchToProps 이것인데 이 아이는 connect 의 second argument 이다.
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  //   console.log(dispatch);
+  //   return { dispatch }; // ! 이제 Home Component 의 props 로 불러와서 dispatch 를 할 수 있게 된다.
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  }; // addToDo 라는 function을 만들었다. 얘는 text argument 를 필요로 하고 이 함수가 실행되면서 dispatch 를 호출한다.
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
+// ! 이 아이는 connect 의 second argument 이다.
+
+// ! export default connect(null, mapDispatchToProps)(Home);
+// mapStateToProps 가 필요 없다면 이렇게 null 로 주는 것도 된다고 한다.
+
+// ! console.log(dispatch) 의 모습.
+// ƒ dispatch(action) {
+//     if (!isPlainObject(action)) {
+//       throw new Error('Actions must be plain objects. ' + 'Use custom middleware for async actions.');
+//     }
+
+//     if (typeof action.type === 'unde…
--- src/store.js
+++ src/store.js
@@ -3,21 +3,22 @@ import { createStore } from "redux";
 const ADD = "ADD";
 const DELETE = "DELETE";
 
-export const addToDo = (text) => {
+// ! action creator 함수 export 를 해제하고 아래 다른 변수에 객체로 담아 export 한다.
+const addToDo = (text) => {
   return {
     type: ADD,
     text,
   };
 };
 
-export const deleteToDo = (id) => {
+const deleteToDo = (id) => {
   return {
     type: DELETE,
     id,
   };
 };
 
-const reducer = (state = ["initVal"], action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD:
       return [...state, { text: action.text, id: Date.now() }];
@@ -30,11 +31,9 @@ const reducer = (state = ["initVal"], action) => {
 
 const store = createStore(reducer);
 
-// ! 변경사항을 알려줬던 subscribe, store 를 subscribe 하게끔.
-// ! state 에 변화가 생길 때마다 App 을 리렌더 하게끔 하자.
-// store 의 변동사항에 대해 subscribe 하려는데.. React-Redux 는 다른 방법을 쓰나?
-// store.subscribe();
-
-// ! index.js 에 가서 현재 App 을 연결시킨다. Store 에다가? 일단 gogo
+export const actionCreators = {
+  addToDo,
+  deleteToDo,
+};
 
 export default store;
